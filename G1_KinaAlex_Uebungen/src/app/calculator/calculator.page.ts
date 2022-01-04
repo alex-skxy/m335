5d96577d0378c5590fef5f1fe1765bf58fdd297c
alex-skxy <alex13skx@gmail.com>
2022-01-04 15:20:02 +0100

    Angular: Directives: implement calculator

--- G1_KinaAlex_Uebungen/src/app/calculator/calculator.page.ts
+++ G1_KinaAlex_Uebungen/src/app/calculator/calculator.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
@@ -6,10 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.page.scss'],
 })
 export class CalculatorPage implements OnInit {
+  readonly actions = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b,
+  };
 
-  constructor() { }
+  firstNumber: number;
+  secondNumber: number;
+  action;
+  input: number;
+  label: string;
+  result: number;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onAction(action) {
+    this.firstNumber = this.input;
+    this.action = action;
+    this.secondNumber = undefined;
+    this.input = undefined;
+    this.result = undefined;
+  }
+
+  onClear() {
+    this.firstNumber = undefined;
+    this.secondNumber = undefined;
+    this.input = undefined;
+    this.action = undefined;
+    this.result = undefined;
+  }
+
+  onEnter() {
+    this.secondNumber = this.input;
+    this.result = this.actions[this.action](this.firstNumber, this.secondNumber);
+    this.input = this.result;
+  }
+
+  // copy pasted from https://stackoverflow.com/a/64809213 :)
+  numericOnly($event: KeyboardEvent) {
+    const pattern = /^([0-9])$/;
+    return pattern.test($event.key);
+  }
 }
