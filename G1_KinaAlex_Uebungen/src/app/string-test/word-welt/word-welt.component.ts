a98ebbcefbd1ef06dd0e79219b51dd712508b9de
alex-skxy <alex13skx@gmail.com>
2022-01-04 13:21:49 +0100

    Angular: Syntax: make the word welt its own component

---
+++ G1_KinaAlex_Uebungen/src/app/string-test/word-welt/word-welt.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-word-welt',
+  templateUrl: './word-welt.component.html',
+  styleUrls: ['./word-welt.component.scss'],
+})
+export class WordWeltComponent implements OnInit {
+  myString = 'Welt';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+
+}
