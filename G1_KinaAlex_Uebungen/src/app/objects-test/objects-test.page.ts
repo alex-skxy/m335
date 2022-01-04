47b492f52c859ee48df37321138d3d063ede2077
alex-skxy <alex13skx@gmail.com>
2022-01-04 13:31:24 +0100

    Angular: Syntax: implement objects page

--- G1_KinaAlex_Uebungen/src/app/objects-test/objects-test.page.ts
+++ G1_KinaAlex_Uebungen/src/app/objects-test/objects-test.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-objects-test',
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./objects-test.page.scss'],
 })
 export class ObjectsTestPage implements OnInit {
+  person: any = {name: 'Rex', vorname: 'Alex'};
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
