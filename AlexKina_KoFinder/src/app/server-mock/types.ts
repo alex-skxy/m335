c878b73374442530eebdd3d0f2e8f1c4e6142d3a
alex-skxy <alex13skx@gmail.com>
2022-01-07 11:32:07 +0100

    KoFinder: server-mock and testdata

---
+++ AlexKina_KoFinder/src/app/server-mock/types.ts
@@ -0,0 +1,30 @@
+export type Activity = {
+  id: string;
+  title: string;
+  type: ActivityType;
+  status: Status;
+  time?: string;
+  people: { person: Person; status: string }[];
+};
+
+export enum Status {
+  'SUGGESTION', 'PLANNED', 'CANCELED'
+}
+
+export type ActivityType = {
+  icon: string;
+  name: string;
+};
+
+export type Person = {
+  id: string;
+  name: string;
+  description: string;
+  phoneNumber: string;
+  avatar: string;
+};
+
+export type Party = {
+  name: string;
+  people: Person[];
+};
