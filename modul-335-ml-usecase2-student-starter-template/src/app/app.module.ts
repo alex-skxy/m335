791369d3501f4cf56a76bad03338136bb4a257ee
alex-skxy <alex13skx@gmail.com>
2022-01-11 09:43:43 +0100

    UseCase2: app module settings and imports

--- modul-335-ml-usecase2-student-starter-template/src/app/app.module.ts
+++ modul-335-ml-usecase2-student-starter-template/src/app/app.module.ts
@@ -1,47 +1,49 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { RouteReuseStrategy } from "@angular/router";
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {RouteReuseStrategy} from "@angular/router";
 
-import {
-  IonicModule,
-  IonicRouteStrategy,
-  MenuController,
-} from "@ionic/angular";
+import {IonicModule, IonicRouteStrategy, MenuController,} from "@ionic/angular";
 
-import { AppComponent } from "./app.component";
-import { AppRoutingModule } from "./app-routing.module";
-import { LogoutComponent } from "./logout/logout.page";
+import {AppComponent} from "./app.component";
+import {AppRoutingModule} from "./app-routing.module";
+import {LogoutComponent} from "./logout/logout.page";
 
 // Import für Ionic Storage
-import { IonicStorageModule } from "@ionic/storage-angular";
+import {IonicStorageModule} from "@ionic/storage-angular";
 
 // Import @angular/fire importieren
-import { AngularFireModule } from "@angular/fire/compat";
-import { AngularFireDatabaseModule } from "@angular/fire/compat/database";
-import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
-import { AngularFireAuthModule } from "@angular/fire/compat/auth";
-import { AngularFireAuthGuard } from "@angular/fire/compat/auth-guard";
-// TODO: Firebase Einstellungen von environments laden
-
-// TODO: Guard für Willkommensseite importieren & zu providers hinzufügen
+import {AngularFireModule} from "@angular/fire/compat";
+import {AngularFireDatabaseModule} from "@angular/fire/compat/database";
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import {AngularFireAuthModule} from "@angular/fire/compat/auth";
+import {AngularFireAuthGuard} from "@angular/fire/compat/auth-guard";
+// welcome guard importieren
+import {WillkommenGuard} from "./_guards/willkommen.guard";
+// Firebase Einstellungen von environments laden
+import {environment} from "../environments/environment"
 
 @NgModule({
-  declarations: [AppComponent, LogoutComponent],
-  entryComponents: [],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    IonicStorageModule.forRoot(),
-    AppRoutingModule,
-    // TODO: Firebase-Imports hinzufügen
-  ],
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    AngularFireAuthGuard,
-    // TODO: Willkommens-Guard hinzufügen
-  ],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, LogoutComponent],
+    entryComponents: [],
+    imports: [
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        BrowserModule,
+        IonicModule.forRoot(),
+        IonicStorageModule.forRoot(),
+        AppRoutingModule,
+        AngularFireModule,
+        AngularFireDatabaseModule,
+        AngularFirestoreModule,
+        AngularFireAuthModule
+    ],
+    providers: [
+        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+        AngularFireAuthGuard,
+        WillkommenGuard
+    ],
+    bootstrap: [AppComponent],
 })
 export class AppModule {
-  constructor(private menuCtrl: MenuController) {}
+    constructor(private menuCtrl: MenuController) {
+    }
 }
