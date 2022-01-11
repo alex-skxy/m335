30570f5a6cb2e341fb7510bbfac950c1f25c6234
alex-skxy <alex13skx@gmail.com>
2022-01-11 10:30:06 +0100

    UseCase2: registration page

--- modul-335-ml-usecase2-student-starter-template/src/app/registrierung/registrierung.page.ts
+++ modul-335-ml-usecase2-student-starter-template/src/app/registrierung/registrierung.page.ts
@@ -1,31 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { AlertController, ToastController } from '@ionic/angular';
-import { User } from '../_types/user';
+import {Component, OnInit} from '@angular/core';
+import {AlertController, ToastController} from '@ionic/angular';
+import {User} from '../_types/user';
 import {Router} from '@angular/router';
+import {AuthService} from "../_services/auth.service";
 
 @Component({
-  selector: 'app-registrierung',
-  templateUrl: './registrierung.page.html',
-  styleUrls: ['./registrierung.page.scss'],
+    selector: 'app-registrierung',
+    templateUrl: './registrierung.page.html',
+    styleUrls: ['./registrierung.page.scss'],
 })
 export class RegistrierungPage implements OnInit {
 
-  user = {} as User;
+    user = {} as User;
 
-  constructor(private router: Router,
-    private alertCtrl: AlertController,
-    private toast: ToastController
-   ) {
-  }
+    constructor(private router: Router,
+                private alertCtrl: AlertController,
+                private toast: ToastController,
+                private authService: AuthService
+    ) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  async register (user: User) {
-    //TODO: Registrierung
-  }
+    async register(user: User) {
+        await this.authService.createUserWithEmailAndPassword(user);
+        await this.router.navigateByUrl('/login');
+    }
 
-  goBackToLogin() {
-   //TODO: Navigation zu Login
-  }
+    async goBackToLogin() {
+        await this.router.navigateByUrl('/login');
+    }
 }
