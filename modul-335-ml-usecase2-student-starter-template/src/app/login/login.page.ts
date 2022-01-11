5bf8f0c1ca5b2a3ed29f2ee4166a61754f8c9b3e
alex-skxy <alex13skx@gmail.com>
2022-01-11 10:01:38 +0100

    UseCase2: loginpage

--- modul-335-ml-usecase2-student-starter-template/src/app/login/login.page.ts
+++ modul-335-ml-usecase2-student-starter-template/src/app/login/login.page.ts
@@ -1,31 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController, AlertController } from '@ionic/angular';
-import { User } from '../_types/user';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {AlertController} from '@ionic/angular';
+import {User} from '../_types/user';
+import {Router} from '@angular/router';
+import {AuthService} from "../_services/auth.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
 
-  user = {} as User;
+    user = {} as User;
 
-  constructor(
-    private alertCtrl: AlertController,
-    private router: Router
-  ) {
+    constructor(
+        private alertCtrl: AlertController,
+        private router: Router,
+        private authService: AuthService
+    ) {
 
-  }
-  ngOnInit() {
-   // TODO: Evt. Willkommensseite darstellen
-  }
-  async doLogin (user: User) {
-   // TODO: Login
-  }
+    }
 
-  gotoRegistrierung () {
-   // TODO: Navigation zu Registrierung
-  }
+    ngOnInit() {
+        // TODO: Evt. Willkommensseite darstellen
+    }
+
+    async doLogin(user: User) {
+        await this.authService.loginWithEmailAndPassword(user);
+    }
+
+    async gotoRegistrierung() {
+        await this.router.navigateByUrl('/registrierung');
+    }
 }
