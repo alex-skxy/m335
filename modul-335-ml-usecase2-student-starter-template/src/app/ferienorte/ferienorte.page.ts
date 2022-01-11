70ccff74e4cd7d9ee6316a637b0a83e7a72ed9ae
alex-skxy <alex13skx@gmail.com>
2022-01-11 11:17:46 +0100

    UseCase2: implement ferienort

--- modul-335-ml-usecase2-student-starter-template/src/app/ferienorte/ferienorte.page.ts
+++ modul-335-ml-usecase2-student-starter-template/src/app/ferienorte/ferienorte.page.ts
@@ -1,27 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { AlertController } from '@ionic/angular';
-import { Observable } from 'rxjs';
-import { AngularFireList, AngularFireDatabase } from '@angular/fire/compat/database';
-import { Ferienort } from "../_types/ferienort";
+import {Component, OnInit} from '@angular/core';
+import {AlertController} from '@ionic/angular';
+import {Observable} from 'rxjs';
+import {AngularFireDatabase, AngularFireList} from '@angular/fire/compat/database';
+import {Ferienort} from "../_types/ferienort";
 
 @Component({
-  selector: 'app-ferienorte',
-  templateUrl: './ferienorte.page.html',
-  styleUrls: ['./ferienorte.page.scss'],
+    selector: 'app-ferienorte',
+    templateUrl: './ferienorte.page.html',
+    styleUrls: ['./ferienorte.page.scss'],
 })
 export class FerienortePage implements OnInit {
 
-  groupNumber: string =  'G0'; // Bsp. G1
+    groupNumber: string = 'G1'; // Bsp. G1
+    private ferienortListRef: AngularFireList<unknown>;
+    ferienortList: Observable<unknown[]>;
 
 
-  constructor(public alertCtrl: AlertController,afDb: AngularFireDatabase) {
-   //TODO: Ferienorte von Firebase holen und zuweisen
-  }
+    constructor(public alertCtrl: AlertController, afDb: AngularFireDatabase) {
+        this.ferienortListRef = afDb.list('/ferienorte/' + this.groupNumber);
+        this.ferienortList = this.ferienortListRef.valueChanges();
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  addFerienort() {
-    //TODO: Ferienort mit AlertCtrl hinzufügen
-  }
+    async addFerienort() {
+        const alert = await this.alertCtrl.create({
+            header: 'Ferienort hinzufügen',
+            inputs: [
+                {name: 'name', type: 'text', placeholder: 'Name Ferienort'}
+            ],
+            buttons: [
+                {
+                    text: 'Abbrechen', role: 'cancel', cssClass: 'secondary'
+                },
+                {
+                    text: 'Hinzufügen',
+                    handler: data => {
+                        this.createFerienOrt({name: data.name});
+                    }
+                }
+            ]
+        })
+        await alert.present();
+    }
+
+    createFerienOrt(ferienort: Ferienort) {
+        this.ferienortListRef.push(ferienort);
+    }
 }
